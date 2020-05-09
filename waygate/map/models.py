770a801d5cd85a9143585185bdc7f57e04322d96
smoogi <spacecorgi@pm.me>
2020-05-09 19:37:16 +0700

    Add model

--- waygate/map/models.py
+++ waygate/map/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Chapter(models.model):
+class Chapter(models.Model):
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     chapter_number = models.PositiveSmallIntegerField()
     chapter_name = models.CharField(max_length=200)
@@ -12,11 +12,18 @@ class Chapter(models.model):
         return self.chapter_name
 
 
-class Book(models.model):
+class Book(models.Model):
     book_number = models.PositiveSmallIntegerField()
     book_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.book_name
 
-class Character(models.model):
+class Character(models.Model):
+    display_name = models.CharField(max_length=200)
+    fullname = models.CharField(max_length=200)
+    color = models.CharField(max_length=7) 
     pass
 
+    def __str__(self):
+        return self.display_name
