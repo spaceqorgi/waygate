c30ae16a67f02848dcf56c1c303d6e10f49224b7
smoogi <spacecorgi@pm.me>
2020-05-12 03:27:06 +0700

    Add models to admin.site.register

--- waygate/map/admin.py
+++ waygate/map/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Book, Chapter, Character, Point
+from .models import Book, Chapter, Character, PointOfView, MapPin
 
 admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(Character)
-admin.site.register(Point)
+admin.site.register(PointOfView)
+admin.site.register(MapPin)
