05703741fe7b574300aeda923098da0ef1682845
smoogi <spacecorgi@pm.me>
2020-05-12 03:26:42 +0700

    Modifty map.urls

--- waygate/map/urls.py
+++ waygate/map/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
+app_name = 'map'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.BookView.as_view(), name='book'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('<int:book_number>/<int:pk>/',
+         views.ChapterView.as_view(), name='chapter'),
 ]
