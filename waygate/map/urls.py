5768e7e6b111259da0daf3cb693c5c6e6e6fe765
smoogi <spacecorgi@pm.me>
2020-05-19 17:54:12 +0700

    Remove no longer needed urls

--- waygate/map/urls.py
+++ waygate/map/urls.py
@@ -5,8 +5,5 @@ from . import views
 app_name = 'map'
 
 urlpatterns = [
-    path('', views.BookView.as_view(), name='book'),
-    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('<int:book_number>/<int:pk>/',
-         views.ChapterView.as_view(), name='chapter'),
+    path('', views.IndexView.as_view(), name='index'),
 ]
