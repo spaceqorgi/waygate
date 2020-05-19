7de20c431a5148316bac960ff3c45d2e879e7e6b
smoogi <spacecorgi@pm.me>
2020-05-19 17:50:11 +0700

    Use these urls to avoid error during runserver

--- waygate/waygate/settings.py
+++ waygate/waygate/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-CORS_ORIGIN_WHITELIST = "http://localhost:3000/"
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080', 
+]
