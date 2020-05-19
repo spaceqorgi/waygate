55509ed3dd4bc63396fbf26d9ae1e53721642f38
smoogi <spacecorgi@pm.me>
2020-05-19 16:12:42 +0700

    Config django to use Postgresql backend

--- waygate/waygate/settings.py
+++ waygate/waygate/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'waygate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'waygate',
+        'USER': 'admin',
+        'PASSWORD': '264135',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
