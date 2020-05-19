9ca29d417932cbd186b6bca412537d376e12d783
smoogi <spacecorgi@pm.me>
2020-05-19 16:41:20 +0700

    Sort models classes

--- waygate/map/models.py
+++ waygate/map/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-class Chapter(models.Model):
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
-    chapter_number = models.PositiveSmallIntegerField()
-    chapter_name = models.CharField(max_length=200)
-    period = models.CharField(max_length=200)
-    summary = models.TextField()
-
-    def __str__(self):
-        return self.chapter_name
-
-
 class Book(models.Model):
     book_number = models.PositiveSmallIntegerField()
     book_name = models.CharField(max_length=200)
@@ -20,9 +9,15 @@ class Book(models.Model):
         return self.book_name
 
 
-class PointOfView(models.Model):
-    chapter = models.ForeignKey('Chapter', on_delete=models.CASCADE)
-    character = models.ForeignKey('Character', on_delete=models.CASCADE)
+class Chapter(models.Model):
+    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+    chapter_number = models.PositiveSmallIntegerField()
+    chapter_name = models.CharField(max_length=200)
+    period = models.CharField(max_length=200)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.chapter_name
 
 
 class Character(models.Model):
@@ -35,9 +30,14 @@ class Character(models.Model):
         return self.display_name
 
 
+class PointOfView(models.Model):
+    chapter = models.ForeignKey("Chapter", on_delete=models.CASCADE)
+    character = models.ForeignKey("Character", on_delete=models.CASCADE)
+
+
 class MapPin(models.Model):
-    chapter_number = models.ForeignKey('Chapter', on_delete=models.CASCADE)
-    point_of_view = models.ForeignKey('PointOfView', on_delete=models.CASCADE)
+    chapter_number = models.ForeignKey("Chapter", on_delete=models.CASCADE)
+    point_of_view = models.ForeignKey("PointOfView", on_delete=models.CASCADE)
     start_x = models.FloatField()
     start_y = models.FloatField()
     end_x = models.FloatField()
