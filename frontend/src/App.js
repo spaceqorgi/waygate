1982a559410d7f070fca2cec8e49c1c9b3b69f70
smoogi <spacecorgi@pm.me>
2020-05-21 17:16:21 +0700

    Modify code style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Canvas extends React.Component {
+  componentDidMount() {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const img = this.refs.image;
 
-    componentDidMount() {
-        const canvas = this.refs.canvas;
-        const ctx = canvas.getContext("2d");
-        const img = this.refs.image;
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      ctx.moveTo(1500, 1291)
+      ctx.lineTo(1531, 1293)
+      ctx.stroke();
+    }
+  }
 
-        img.onload = () => {
-            ctx.drawImage(img, 0, 0);
-            ctx.moveTo(1530, 1294)
-            ctx.lineTo(1571, 1187)
-            ctx.lineTo(1844, 1263)
-            ctx.stroke();
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas" width={3374} height={2427} />
+        <img ref="image" alt="map" src={require('./images/map-big-clean.png')} className="hidden" />
+      </div>
+    );
+  }
+}
+
         }
     }
     
