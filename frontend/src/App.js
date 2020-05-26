b5068b9d3a825ead361cbea2ebf5f5a7b014dbf2
smoogi <spacecorgi@pm.me>
2020-05-26 18:51:39 +0700

    Canvas is now Chapter's child

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { MapInteractionCSS } from 'react-map-interaction';
 
 class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      narrators: this.props.narrators
+    }
+  }
 
   componentDidMount() {
     const canvas = this.refs.canvas;
@@ -14,27 +20,77 @@ class Canvas extends React.Component {
     img.onload = () => {
       ctx.drawImage(img, 0, 0);
       ctx.lineWidth = 3;
+    }
+  }
 
-      for (const narrator of this.props.narrators){
-        ctx.lineTo(narrator.points.x, narrator.points.y)
+  shouldComponentUpdate(nextProps){
+    return nextProps.narrators !== this.state.narrators;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.setState({
+      narrators: this.props.narrators
+    });
+
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    const img = this.refs.image;
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(img, 0, 0);
+    ctx.lineWidth = 3;
+
+    let narrators = this.props.narrators;
+    if(narrators !== {}){
+      console.log("DEBUG: You select a chapter");
+      console.log(narrators);
+      for (const [id, narrator] of Object.entries(narrators)){
+        let isFirstPoint = true;
+        for (const [id, point] of Object.entries(narrator.points)){
+          if(isFirstPoint){
+            ctx.moveTo(point.x, point.y)
+            isFirstPoint = false;
+            ctx.beginPath();
+            ctx.rect(point.x, point.y, 10, 10)
+            ctx.stroke();
+          } else {
+            ctx.lineTo(point.x, point.y);
+          }
+          console.log(point);
+        }
         ctx.stroke();
       }
+    } else {
+      console.log("DEBUG: There is no current selected Chapter!");
     }
+
   }
 
   render() {
     let height = 3374;
     let width = 2427;
     return (
-      <div className>
+      <div>
         <canvas ref="canvas" width={height} height={width} />
-        <Image fluid ref="image" alt="map" src={require('./images/map-big-clean.png')} className="hidden" />
+        <img ref="image" alt="map" src={require('./images/map-big-clean.png')} className="hidden" />
       </div>
     );
   }
 }
 
 class Chapter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentChapter: {},
+    }
+  }
+
+  onChapterSelected(chapter) {
+    console.log(chapter)
+    this.setState({
+      currentChapter: chapter
+    })
+  }
 
   render() {
     const { error, isLoaded, items } = this.props;
@@ -44,21 +100,43 @@ class Chapter extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <Accordion>
-          {items.map(item => (
-          <Card key={item.chapter_number}>
-            <Accordion.Toggle as={Card.Header} eventKey={item.chapter_number}>
-              {item.chapter_number} {item.chapter_name}
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey={item.chapter_number}>
-              <Card.Body>
-                <strong>{item.period}</strong>
-                <p>{item.summary}</p>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-          ))}
-        </Accordion>
+          <Row>
+            <Col lg={9} md={12}>
+              <MapInteractionCSS
+                  scale={0.30}
+                  defaultScale={0.35}
+                  height="800px"
+                  width="100%"
+              >
+                <Canvas
+                  narrators={this.state.currentChapter.narrators}
+                />
+              </MapInteractionCSS>
+            </Col>
+            <Col lg={3} md={6} className="Chapter">
+            <Accordion>
+              {items.map(item => (
+                  <Card key={item.chapter_number}>
+                    <Accordion.Toggle
+                        as={Card.Header}
+                        eventKey={item.chapter_number}
+                        onClick={(i) => this.onChapterSelected(item)}
+                    >
+                      Ch. {item.chapter_number}: {item.chapter_name}
+                    </Accordion.Toggle>
+                    <Accordion.Collapse
+                        eventKey={item.chapter_number}
+                    >
+                      <Card.Body>
+                        <strong>{item.period}</strong>
+                        <p>{item.summary}</p>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+              ))}
+            </Accordion>
+            </Col>
+          </Row>
       )
     }
   }
@@ -93,12 +171,12 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
     }
   }
 
   componentDidMount() {
-    let url = "http://127.0.0.1:8000/api/book"
+    let url = "http://127.0.0.1:8000/api/chapter"
 
     fetch(url)
         .then(res => res.json())
@@ -121,35 +199,11 @@ class App extends Component {
   render() {
     return (
       <Container fluid className="App">
-        <Row>
-          <Col lg={9} md={12}>
-            <MapInteractionCSS
-              scale={0.30}
-              defaultScale={0.35}
-              height="800px"
-              width="100%"
-            >
-              <Canvas
-                narrators={this.state.items.chapters.narrators}
-              />
-            </MapInteractionCSS>
-          </Col>
-          <Col lg={3} md={6} className="Chapter">
             <Chapter
               error={this.state.error}
               isLoaded={this.state.isLoaded}
-              chapter={this.state.items.chapters}
+              items={this.state.items}
             />
-          </Col>
-          // TODO Perhaps remove this
-          <Col lg={3} md={6} className="Book">
-            <Book
-                error={this.state.error}
-                isLoaded={this.state.isLoaded}
-                items={this.state.items}
-            />
-          </Col>
-        </Row>
       </Container>
     );
   }
